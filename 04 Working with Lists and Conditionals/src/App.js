511dfc2962dae22b2296ff177ae0bca306b95400
Leonard Dalmacio <leonarddalmacio@Aracari.local>
2018-05-11 08:55:09 +0800

    Finshed Lesson 4?

---
+++ 04 Working with Lists and Conditionals/src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: '1', name: 'Max', age: '28'},
+      {id: '2', name: 'Manu', age: '29'},
+      {id: '3', name: 'Stephanie', age: '26'},
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = () => { 
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice(); // alternative to copying array into a variable
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+            />
+          })}
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App</h1>
+        <p>This is really working!</p>
+
+        {/* This is inefficient */}
+        <button
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Person</button>
+
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
