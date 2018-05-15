6c236a75451b61efb189b5c334846f11602f473e
Leonard Dalmacio <leonarddalmacio@Aracari.local>
2018-05-15 13:11:11 +0800

    Finished Module/Lesson 05

--- 05 Styling React Components & Elements/src/App.js
+++ 05 Styling React Components & Elements/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -38,20 +38,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -68,30 +56,26 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      }
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
 
           {/* This is inefficient */} 
           <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Person</button>
           {persons}
         </div>
